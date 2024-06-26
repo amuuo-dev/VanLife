56a048f99243824066c774cbf992b9cad28811c9
amuuo-dev <anthonmuuo92@gmail.com>
2024-06-26 13:28:28 +0300

    project van data in van page and style the page

--- components/Vans.jsx
+++ components/Vans.jsx
@@ -5,12 +5,27 @@ const Vans = () => {
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
-      .then((data) => setVans(data));
+      .then((data) => setVans(data.vans));
   }, []);
-  console.log(vans);
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          {van.price}
+          <span>/day</span>
+        </p>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </div>
+    </div>
+  ));
+
   return (
-    <div>
-      <h1>vans goes here</h1>
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vanElements}</div>
     </div>
   );
 };
