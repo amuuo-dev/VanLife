f15a5fa65acc284ec1e0a8fe6b6acc091723681f
amuuo-dev <anthonmuuo92@gmail.com>
2024-06-27 09:24:51 +0300

    add a vanDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "../components/Home";
 import About from "../components/About";
 import Vans from "../components/Vans";
 import "../server"; // Import the server
+import VanDetail from "../components/VanDetail";
 const App = () => {
   return (
     <div>
@@ -13,12 +14,14 @@ const App = () => {
           </Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
         </Routes>
       </BrowserRouter>
     </div>
