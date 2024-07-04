9d757aa85051726f362cf122bc3883b9700b1aeb
amuuo-dev <anthonmuuo92@gmail.com>
2024-07-04 11:26:22 +0300

    set up hostvandetail page and back to all vans link

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,8 +1,39 @@
+import { useParams, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 const HostVanDetail = () => {
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, [id]);
+
+  if (!currentVan) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>this is the van id detail</h1>
-    </div>
+    <section>
+      <Link to=".." relative="path" className="back-button">
+        &larr; <span>Back to all vans</span>
+      </Link>
+
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
