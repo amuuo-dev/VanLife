aba36c244510dd532d2f289fcebfb28d97fd7afa
amuuo-dev <anthonmuuo92@gmail.com>
2024-07-04 14:49:24 +0300

    nest the route in host van

---
+++ pages/Host/HostVanPhotos.jsx
@@ -0,0 +1,7 @@
+import { useOutletContext } from "react-router-dom";
+const HostVanPhotos = () => {
+  const { currentVan } = useOutletContext();
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
+};
+
+export default HostVanPhotos;
