fa16d0290c2d330d5ecd2361eba88fe255bfd24a
amuuo-dev <anthonmuuo92@gmail.com>
2024-07-09 10:11:30 +0300

    create a not found page

--- pages/Vans/VanDetail.jsx
+++ pages/Vans/VanDetail.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useLocation } from "react-router-dom";
 const VanDetail = () => {
+  const location = useLocation();
+
   const params = useParams();
   const [van, setVan] = useState(null);
   useEffect(() => {
@@ -8,8 +10,15 @@ const VanDetail = () => {
       .then((res) => res.json())
       .then((data) => setVan(data.vans));
   }, [params.id]);
+
+  const search = location.state?.search || "";
+  const type = location.state?.type || "all";
+
   return (
     <div className="van-detail-container">
+      <Link to={`..${search}`} relative="path" className="back-button">
+        &larr; <span>Back to {type} vans</span>
+      </Link>
       {van ? (
         <div className="van-detail">
           <img src={van.imageUrl} />
