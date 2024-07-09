6a49842db61955daff1b48dc0700eb57c4cfe7f0
amuuo-dev <anthonmuuo92@gmail.com>
2024-07-09 12:51:29 +0300

    create a login page

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
 const Footer = () => {
   return (
     <div>
-      <footer>&#169; 2024 #VANLIFE</footer>
+      <footer>
+        &#169; 2024{" "}
+        <a
+          href="https://www.linkedin.com/in/anthony-muuo/"
+          target="_blank"
+          rel="no_opener"
+        >
+          #VANLIFE
+        </a>
+      </footer>
     </div>
   );
 };
