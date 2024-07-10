971f24d7c9c2097c63efc560dc41699f2626abfb
amuuo-dev <anthonmuuo92@gmail.com>
2024-07-10 09:16:03 +0300

    inialize good ui in the login page when one clicks host

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 const Login = () => {
   const [loginFormData, setLoginFormData] = useState({
     email: "",
     password: "",
   });
+  const location = useLocation();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -20,6 +22,9 @@ const Login = () => {
 
   return (
     <div className="login-container">
+      {location.state?.message && (
+        <h3 className="login-first">{location.state.message}</h3>
+      )}
       <h1>Sign in to your account</h1>
       <form onSubmit={handleSubmit} className="login-form">
         <input
